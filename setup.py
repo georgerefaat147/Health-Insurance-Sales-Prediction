23b99a1b7e1322cacba3dd062ba175ea177590b7
George Refaat <georgerefaat147@gmail.com>
2023-11-18 17:25:06 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting if the customer for heathe insurance company is going to respond for a vehicle insurance',
+    author='Your name (or your organization/company/team)',
+    license='',
+)
